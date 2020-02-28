4f51fb831aeb89c52554b91fe38609babc8f6083
Steven Perfors <stevecanbuild@gmail.com>
2020-02-28 22:10:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,5 +9,5 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each {|name, age| age < 12 puts "Happy Birthday #{name}! You are now #{age} years old!"}
+  birthday_kids.each {|name, age| age < 12, puts "Happy Birthday #{name}! You are now #{age} years old!"}
 end
